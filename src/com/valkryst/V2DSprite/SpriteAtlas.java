4ca0ca829e9bc9e3623ae2dd74996bcb0cddd6f3
Valkryst <valkryst@valkryst.com>
2019-02-22 11:11:45 -0400

    Adds the createSpriteAtlas function to handle loading, from both the Jar and filesystem, of sprite atlases.
    Rewrites the SpriteAtlas, so the user must go through the createSpriteAtlas function to construct a new SpriteAtlas.
    
    Adds caching to loaded SpriteAtlases, so the user isn't able to stress the system by re-loading the same atlas from disk over and over.

--- src/com/valkryst/V2DSprite/SpriteAtlas.java
+++ src/com/valkryst/V2DSprite/SpriteAtlas.java
@@ -1,5 +1,7 @@
 package com.valkryst.V2DSprite;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.NonNull;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -10,32 +12,56 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.VolatileImage;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class SpriteAtlas {
+    /** The cache of recently loaded atlases. */
+    private final static Cache<Integer, SpriteAtlas> ATLAS_CACHE = Caffeine.newBuilder().initialCapacity(1).expireAfterAccess(5, TimeUnit.MINUTES).build();
+
     /** The image loaded from disk. */
     private final BufferedImage bufferedAtlasImage;
     /** The image used to draw with. */
     private VolatileImage volatileAtlasImage;
     /** The sprite sheets contained on the atlas image. */
-    private final HashMap<String, SpriteSheet> spriteSheets = new HashMap<>();
+    private final HashMap<String, SpriteSheet> spriteSheets;
 
     /**
      * Constructs a new SpriteAtlas.
      *
-     * Closes streams after use.
+     * @param atlas
+     *          The atlas.
      *
-     * @param atlasImageStream
-     *          The input stream for the atlas' image.
+     * @param spriteSheets
+     *          The sprite sheets within the atlas.
+     */
+    private SpriteAtlas(final BufferedImage atlas, final HashMap<String, SpriteSheet> spriteSheets) {
+        this.bufferedAtlasImage = atlas;
+        volatileAtlasImage = convertToVolatileImage(bufferedAtlasImage);
+        this.spriteSheets = spriteSheets;
+    }
+
+    /**
+     * Creates a new SpriteAtlas.
      *
-     * @param atlasJSONStream
-     *          The input stream for the atlas' JSON data.
+     * @param pngPath
+     *          The file path of the atlas' image.
+     *
+     * @param jsonPath
+     *          The file path of the atlas' JSON data.
+     *
+     * @return
+     *          The sprite atlas.
+     *
+     * @throws FileNotFoundException
+     *          If the PNG/JSON path is empty.
+     *          If the PNG/JSON path points to a directory.
+     *          If the PNG/JSON file cannot be found within the Jar or on the filesystem.
      *
      * @throws NullPointerException
-     *          If the either stream is null.
+     *          If the either file path is null.
      *
      * @throws IOException
      *          If an error occurs during reading or when not able to create required ImageInputStream.
@@ -44,32 +70,108 @@ public class SpriteAtlas {
      *          If the size of the loaded image is greater than 2048x2048.
      *
      * @throws ParseException
-     *          If an error occurs during parsing of the JSON file.
+     *          If there's an error when parsing the JSON.
      */
-    public SpriteAtlas(final @NonNull InputStream atlasImageStream, final @NonNull InputStream atlasJSONStream) throws IOException, ParseException {
+    public SpriteAtlas createSpriteAtlas(final @NonNull String pngPath, final @NonNull String jsonPath) throws IOException, ParseException {
+        // Check cache for atlas.
+        final int hash = Objects.hash(pngPath, jsonPath);
+        final SpriteAtlas cachedAtlas = ATLAS_CACHE.getIfPresent(hash);
+
+        if (cachedAtlas != null) {
+            return cachedAtlas;
+        }
+
+        // Check for empty paths.
+        if (pngPath.isEmpty()) {
+            throw new FileNotFoundException("The PNG path cannot be empty.");
+        }
+
+        if (jsonPath.isEmpty()) {
+            throw new FileNotFoundException("The JSON path cannot be empty.");
+        }
+
+        // Attempt to load from Jar.
+        InputStream pngInputStream = SpriteAtlas.class.getClassLoader().getResourceAsStream(pngPath);
+        InputStream jsonInputStream = SpriteAtlas.class.getClassLoader().getResourceAsStream(jsonPath);
+
+        if (pngInputStream != null && jsonInputStream != null) {
+            // Load Buffered Image:
+            final BufferedImage bufferedAtlasImage = ImageIO.read(pngInputStream); // IO Exception
+
+            if (bufferedAtlasImage.getWidth() > 2048 || bufferedAtlasImage.getHeight() > 2048) {
+                throw new IllegalArgumentException("The image used by the sprite atlas should not exceed 2048x2048 in size.");
+            }
+
+            pngInputStream.close(); // IO Exception
+
+            // Load JSON Data:
+            final JSONParser parser = new JSONParser();
+            final JSONObject atlasData = (JSONObject) parser.parse(new InputStreamReader(jsonInputStream));
+
+            final HashMap<String, SpriteSheet> spriteSheets = new HashMap<>();
+            final JSONArray sheetDataArray = (JSONArray) atlasData.get("Sheets");
+            sheetDataArray.forEach(sheetData -> {
+                final SpriteSheet sheet = new SpriteSheet(this, (JSONObject) sheetData);
+                spriteSheets.put(sheet.getName(), sheet);
+            });
+
+            jsonInputStream.close();
+
+            // Create the atlas, add it to the cache, and return it.
+            final SpriteAtlas atlas = new SpriteAtlas(bufferedAtlasImage, spriteSheets);
+            ATLAS_CACHE.put(hash, atlas);
+            return atlas;
+        }
+
+        // Attempt to load from filesystem.
+        final File pngFile = new File(pngPath);
+        final File jsonFile = new File(jsonPath);
+
+        if (pngFile.isDirectory()) {
+            throw new FileNotFoundException("The PNG file path '" + pngPath + "' points to a directory.");
+        }
+
+        if (jsonFile.isDirectory()) {
+            throw new FileNotFoundException("The JSON file path '" + jsonPath + "' points to a directory.");
+        }
+
+        if (!pngFile.exists()) {
+            throw new FileNotFoundException("The PNG file '" + pngPath + "' does not exist.");
+        }
+
+        if (!jsonFile.exists()) {
+            throw new FileNotFoundException("The JSON file '" + jsonPath + "' does not exist.");
+        }
+
+        pngInputStream = new FileInputStream(pngFile);
+        jsonInputStream = new FileInputStream(jsonFile);
+
         // Load Buffered Image:
-        bufferedAtlasImage = ImageIO.read(atlasImageStream);
+        final BufferedImage bufferedAtlasImage = ImageIO.read(pngInputStream); // IO Exception
 
         if (bufferedAtlasImage.getWidth() > 2048 || bufferedAtlasImage.getHeight() > 2048) {
             throw new IllegalArgumentException("The image used by the sprite atlas should not exceed 2048x2048 in size.");
         }
 
-        volatileAtlasImage = convertToVolatileImage(bufferedAtlasImage);
-        atlasImageStream.close();
+        pngInputStream.close(); // IO Exception
 
         // Load JSON Data:
         final JSONParser parser = new JSONParser();
-        final JSONObject atlasData = (JSONObject) parser.parse(new InputStreamReader(atlasJSONStream));
+        final JSONObject atlasData = (JSONObject) parser.parse(new InputStreamReader(jsonInputStream));
 
+        final HashMap<String, SpriteSheet> spriteSheets = new HashMap<>();
         final JSONArray sheetDataArray = (JSONArray) atlasData.get("Sheets");
         sheetDataArray.forEach(sheetData -> {
             final SpriteSheet sheet = new SpriteSheet(this, (JSONObject) sheetData);
             spriteSheets.put(sheet.getName(), sheet);
         });
 
-        // Close Streams:
-        atlasImageStream.close();
-        atlasJSONStream.close();
+        jsonInputStream.close();
+
+        // Create the atlas, add it to the cache, and return it.
+        final SpriteAtlas atlas = new SpriteAtlas(bufferedAtlasImage, spriteSheets);
+        ATLAS_CACHE.put(hash, atlas);
+        return atlas;
     }
 
     /**
