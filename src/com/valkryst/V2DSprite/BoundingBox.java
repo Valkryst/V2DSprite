a452feba644e93f6669fa219a3260e71ca220021
Valkryst <valkryst@valkryst.com>
2019-03-04 20:22:16 -0400

    Adds a draw function and re-adds the intersection functions.

--- src/com/valkryst/V2DSprite/BoundingBox.java
+++ src/com/valkryst/V2DSprite/BoundingBox.java
@@ -29,4 +29,46 @@ public class BoundingBox {
         final int height = VJSON.getInt(data, "height");
         bounds = new Rectangle(x, y, width, height);
     }
+
+    /**
+     * Draws the bounding box on a graphics context.
+     *
+     * @param gc
+     *          The graphics context.
+     *
+     * @param xOffset
+     *          The x-axis position of the sprite, associated with the bounding box, on the graphics context.
+     *
+     * @param yOffset
+     *          The y-axis position of the sprite, associated with the bounding box, on the graphics context.
+     */
+    public void draw(final Graphics2D gc, final int xOffset, final int yOffset) {
+        gc.drawRect(xOffset + bounds.x, yOffset + bounds.y, bounds.width, bounds.height);
+    }
+
+    /**
+     * Determines whether the this bounding box intersects another.
+     *
+     * @param other
+     *          The other bounding box.
+     *
+     * @return
+     *          Whether this bounding box intersects another.
+     */
+    public boolean intersects(final BoundingBox other) {
+        return other.getBounds().intersects(bounds);
+    }
+
+    /**
+     * Determines whether the this bounding box intersects another.
+     *
+     * @param other
+     *          The other bounding box.
+     *
+     * @return
+     *          Whether this bounding box intersects another.
+     */
+    public boolean intersects(final Rectangle other) {
+        return other.intersects(bounds);
+    }
 }
