4d3985451e2b7602be1af01e3712618037e2e669
Valkryst <valkryst@valkryst.com>
2019-03-04 22:00:31 -0400

    Removes the intersection functions.

--- src/com/valkryst/V2DSprite/BoundingBox.java
+++ src/com/valkryst/V2DSprite/BoundingBox.java
@@ -29,30 +29,4 @@ public class BoundingBox {
         final int height = VJSON.getInt(data, "height");
         bounds = new Rectangle(x, y, width, height);
     }
-
-    /**
-     * Determines whether the this bounding box intersects another.
-     *
-     * @param other
-     *          The other bounding box.
-     *
-     * @return
-     *          Whether this bounding box intersects another.
-     */
-    public boolean intersects(final BoundingBox other) {
-        return other.getBounds().intersects(bounds);
-    }
-
-    /**
-     * Determines whether the this bounding box intersects another.
-     *
-     * @param other
-     *          The other bounding box.
-     *
-     * @return
-     *          Whether this bounding box intersects another.
-     */
-    public boolean intersects(final Rectangle other) {
-        return other.intersects(bounds);
-    }
 }
